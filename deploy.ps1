67841c4fbecc3c933a2128ea7f11c86bb1ac2b3f
Hazuli Fidastian <hazulifidastian@gmail.com>
2021-02-15 14:46:42 +0700

    Updating site

---
+++ deploy.ps1
@@ -0,0 +1,15 @@
+Read-Host -Prompt ("Inisiasi Hugo website" `
+	+ ". Tekan tombol apa saja untuk melanjutkan, atau ctrl+c untuk membatalkan")
+
+# Commit and push source code
+git add .
+git commit -m "Updating site"
+git push origin master
+
+# Build the project.
+hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+# On public folder, Commit and push
+git -C public add .
+git -C public commit -m "Rebuilding site"
+git push origin master
